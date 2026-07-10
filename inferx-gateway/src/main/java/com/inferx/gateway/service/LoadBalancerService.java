4e02ea3f8738c980a9d61dcfc97f986852de6441
divyanshu.yadav <divyanshuyadav715@gmail.com>
2026-07-10 23:13:14 +0530

    feat: Complete InferX architecture and polish

---
+++ inferx-gateway/src/main/java/com/inferx/gateway/service/LoadBalancerService.java
@@ -0,0 +1,4 @@
+package com.inferx.gateway.service;
+
+// This service is obsolete and has been removed.
+// We keep this empty file to overwrite the old one on GitHub since our push script doesn't handle deletions natively.
